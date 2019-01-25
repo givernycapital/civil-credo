b2f1084a8f1e2191c0080ad5a13b1ce720e7e3e8
Nicolas Charlery <nicolas@bokay.io>
2019-01-25 16:35:03 -0500

    Test tag wip checker

---
+++ test/check/design/tag_wip_test.exs
@@ -0,0 +1,22 @@
+defmodule CivilCredo.Check.Design.TagWipTest do
+  use Credo.TestHelper
+
+  @described_check CivilCredo.Check.Design.TagWip
+
+  describe "tag wip" do
+    test "finds tag wip" do
+      string = """
+        defmodule TestModule do
+            @tag :wip
+            def hello(), do: :world
+        end
+      """
+
+      string
+      |> to_source_file
+      |> assert_issue(@described_check, fn issue ->
+        issue.message == "This test has been tagged with WIP"
+      end)
+    end
+  end
+end
