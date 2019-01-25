4a84d42cf6446ccffa9afb697ff4b6b60de6c804
Nicolas Charlery <nicolas@bokay.io>
2019-01-25 16:33:18 -0500

    Remove generated code

--- lib/civil_credo.ex
+++ lib/civil_credo.ex
@@ -1,18 +1,2 @@
 defmodule CivilCredo do
-  @moduledoc """
-  Documentation for CivilCredo.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CivilCredo.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/civil_credo_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CivilCredoTest do
-  use ExUnit.Case
-  doctest CivilCredo
-
-  test "greets the world" do
-    assert CivilCredo.hello() == :world
-  end
-end
